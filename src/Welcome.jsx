773e40a1332e9f36a9fba58dfbe1cb80cd1974be
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-07-13 11:28:00 +0200

    changed buttons

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,22 +4,25 @@ import { NavLink } from 'react-router-dom';
 const Welcome = () => {
   return (
     <>
-      <div className="flex-1 p-6 text-center border-2 border-gray-300 rounded-md">
-        <NavLink
+      <NavLink
+        to="/new-client"
+        className="flex-1 p-6 text-center border-2 border-gray-300 rounded-md"
+      >
+        <div
           to="/new-client"
           className="text-lg sm:text-xl font-semibold text-gray-700"
         >
           Neuer Kunde
-        </NavLink>
-      </div>
-      <div className="flex-1 p-6 text-center border-2 border-gray-300 rounded-md">
-        <NavLink
-          to="/clients"
-          className="text-lg sm:text-xl font-semibold text-gray-700"
-        >
+        </div>
+      </NavLink>
+      <NavLink
+        to="/clients"
+        className="flex-1 p-6 text-center border-2 border-gray-300 rounded-md"
+      >
+        <div className="text-lg sm:text-xl font-semibold text-gray-700">
           Bestehende Kunden
-        </NavLink>
-      </div>
+        </div>
+      </NavLink>
     </>
   );
 };
