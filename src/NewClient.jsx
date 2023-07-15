5a846adb4f295b28b56d924bda6fa62aa55b99b5
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-07-15 09:57:32 +0200

    safe-bottom

--- src/NewClient.jsx
+++ src/NewClient.jsx
@@ -40,7 +40,7 @@ const NewClient = () => {
 
   return (
     <Form method="post" className="w-full min-h-screen">
-      <div className="p-6 min-h-screen">
+      <div className="p-6 min-h-screen safe-bottom">
         <StepContent
           currentStep={currentStep}
           formContent={formContent}
