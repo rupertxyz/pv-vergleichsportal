7ab5fc714d88a931031641eeae0f7eacb42ef7b9
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-07-17 08:09:37 +0200

    maps change

--- src/components/FormComponents/Address.jsx
+++ src/components/FormComponents/Address.jsx
@@ -8,6 +8,7 @@ export default function Input({
   identifier,
 }) {
   const value = formContent[identifier] || '';
+  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
   return (
     <div className="w-full sm:w-1/2 p-2">
       <label className="block text-sm font-medium leading-6 text-gray-900 truncate">
@@ -15,7 +16,7 @@ export default function Input({
       </label>
       <div className="relative mt-1 rounded-md shadow-sm">
         <Autocomplete
-          apiKey="AIzaSyDIdu0Oc_4tqPrd-Sds13hN8Xucb3rgb_0"
+          apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
           onPlaceSelected={(place) => {
             console.log(place);
           }}
@@ -27,8 +28,6 @@ export default function Input({
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
           style={{ paddingLeft: '12px' }}
           placeholder={placeholder}
-          value={value}
-          onChange={(e) => console.log(e)}
         />
       </div>
     </div>
