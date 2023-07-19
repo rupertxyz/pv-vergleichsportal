14df8ac9fcc1f8d05a33228a3543918ba696d693
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-07-19 18:03:16 +0200

    date fix to German

--- src/components/FormComponents/DatePicker.jsx
+++ src/components/FormComponents/DatePicker.jsx
@@ -62,6 +62,8 @@ const DatePicker = ({ label, formContent, setFormContent, identifier }) => {
           inputClassName="w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
           placeholder="Datum auswählen"
           readOnly={true}
+          i18n="de"
+          displayFormat="DD.MM.YYYY"
         />
       </div>
     </div>
