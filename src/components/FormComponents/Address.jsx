e173d8f37fda0a752ef5bc8c985e61b3af7c9f03
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-07-19 12:00:43 +0200

    address fix

--- src/components/FormComponents/Address.jsx
+++ src/components/FormComponents/Address.jsx
@@ -1,8 +1,5 @@
-import { useRef, useState } from 'react';
-import { StandaloneSearchBox, LoadScript } from '@react-google-maps/api';
+import { useEffect, useRef, useState } from 'react';
 import './Address.css';
-
-const libraries = ['places'];
 
 export default function Input({
   label,
@@ -11,17 +8,43 @@ export default function Input({
   setFormContent,
   identifier,
 }) {
-  const inputRef = useRef();
+  const autocompleteInputRef = useRef(null);
   const value = formContent[identifier] || '';
-  const [isFocused, setIsFocused] = useState(false);
+  const [googleMapsLoaded, setGoogleMapsLoaded] = useState(false);
 
-  const handlePlaceChanged = () => {
-    const [place] = inputRef.current.getPlaces();
-    if (place) {
-      console.log(place);
-      console.log(place.formatted_address);
+  useEffect(() => {
+    // Only load the script if it's not already loaded
+    if (!window.google) {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`;
+      script.async = true;
+      script.defer = true;
+      window.initMap = () => setGoogleMapsLoaded(true);
+      document.body.appendChild(script);
+
+      // Clean up on component unmount
+      return () => {
+        document.body.removeChild(script);
+      };
+    } else {
+      setGoogleMapsLoaded(true);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    if (googleMapsLoaded) {
+      const autocomplete = new window.google.maps.places.Autocomplete(
+        autocompleteInputRef.current
+      );
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        console.log(place);
+        console.log(place.formatted_address);
+        // setFormContent({ ...formContent, [identifier]: place.formatted_address });
+      });
+    }
+  }, [googleMapsLoaded]); // Add googleMapsLoaded dependency
 
   return (
     <div className="w-full sm:w-1/2 p-2">
@@ -29,63 +52,17 @@ export default function Input({
         {label}
       </label>
       <div className="relative mt-1 rounded-md shadow-sm">
-        <LoadScript
-          googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-          libraries={libraries}
-        >
-          <StandaloneSearchBox
-            onLoad={(ref) => (inputRef.current = ref)}
-            onPlacesChanged={handlePlaceChanged}
-          >
-            <input
-              name={identifier}
-              value={value}
-              onChange={(e) =>
-                setFormContent({ ...formContent, [identifier]: e.target.value })
-              }
-              className="input"
-              style={{
-                display: 'block',
-                width: '100%',
-                borderRadius: '.375rem',
-                border: 'none',
-                padding: '.5rem .75rem',
-                color: '#000',
-                boxShadow: isFocused
-                  ? '0 0 0 2px #4b5563 inset' // focus:ring-2 focus:ring-inset focus:ring-gray-600
-                  : '0 0 0 1px #d1d5db inset', // ring-1 ring-inset ring-gray-300
-                fontSize: '.875rem',
-                lineHeight: '1.5',
-                outline: 'none',
-              }}
-              placeholder={placeholder}
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-            />
-          </StandaloneSearchBox>
-        </LoadScript>
-        {/* <Autocomplete
-          apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-          onPlaceSelected={(place) => {
-            console.log(place);
-          }}
-          options={{
-            types: ['address'],
-            componentRestrictions: { country: 'de' },
-          }}
-          language="de"
-          className="block w-full rounded-md border-0 p-1.5 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6"
-          style={{
-            paddingLeft: '12px',
-            outline: 'none',
-            boxShadow: isFocused
-              ? '0 0 0 2px #4b5563 inset'
-              : '0 0 0 1px #d1d5db inset',
-          }}
+        <input
+          type="text"
+          name={identifier}
+          value={value}
+          ref={autocompleteInputRef}
+          onChange={(e) =>
+            setFormContent({ ...formContent, [identifier]: e.target.value })
+          }
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
           placeholder={placeholder}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        /> */}
+        />
       </div>
     </div>
   );
