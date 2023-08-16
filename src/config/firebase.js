d0e09406dd7734ad169fb1f075f933a4abeede21
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-08-16 18:31:25 +0200

    firebase secret change

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-import { firebaseConfig } from '../.firebaseConfig';
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'pv-vergleichsportal.firebaseapp.com',
+  projectId: 'pv-vergleichsportal',
+  storageBucket: 'pv-vergleichsportal.appspot.com',
+  messagingSenderId: '32551160353',
+  appId: '1:32551160353:web:3a0153fd6ec759fc43b160',
+};
 
 const app = initializeApp(firebaseConfig);
 
