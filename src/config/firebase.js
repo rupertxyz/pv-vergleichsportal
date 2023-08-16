e824a177e3ba80df33761aed28b7db73e5ad3883
Rupert Hoffschmidtm <rupert.hoffschmidt@gmail.com>
2023-08-16 18:19:56 +0200

    firebase update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDQeSSfQ6R1WBupJAh7LV3o-W9MrlQtImE',
-  authDomain: 'pv-vergleichsportal.firebaseapp.com',
-  projectId: 'pv-vergleichsportal',
-  storageBucket: 'pv-vergleichsportal.appspot.com',
-  messagingSenderId: '32551160353',
-  appId: '1:32551160353:web:3a0153fd6ec759fc43b160',
-};
+import { firebaseConfig } from '../.firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 
